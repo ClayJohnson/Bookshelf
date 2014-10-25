627ad19e59cadff66f1cde6eb9f574ec22e1e6bd
ClayJohnson <imm589@my.utsa.edu>
2014-10-24 20:01:32 -0500

    Created CategoryDAO to implement CRUD operations on Categories.

---
+++ BookshelfNew/src/project/bookshelfnew/CategoryDAO.java
@@ -0,0 +1,119 @@
+/**
+ * 
+ */
+package project.bookshelfnew;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.util.Log;
+
+/**
+ * CategoryDAO is a DAO which extends BookshelfDBDAO to implement CRUD
+ * operations for Categories.
+ * 
+ * @author Clay
+ * 
+ */
+public class CategoryDAO extends BookshelfDBDAO {
+
+	// where clauses for database queries
+	private static final String WHERE_ID_EQUALS = MySQLiteHelper.COLUMN_ID
+			+ " =?";
+
+	// Database fields
+	private String[] allColumns = { MySQLiteHelper.COLUMN_ID,
+			MySQLiteHelper.CATEGORY_NAME };
+
+	/**
+	 * @param context
+	 */
+	public CategoryDAO(Context context) {
+		super(context);
+	}
+	
+	/**
+	 * Inserts a category into the table.
+	 * 
+	 * @param category
+	 *            the category to be inserted
+	 * @return the ID of the newly inserted category, or -1 if an error occurred
+	 */
+	public long save(Category category) {
+		ContentValues values = new ContentValues();
+		values.put(MySQLiteHelper.CATEGORY_NAME, category.getName());
+
+		return database.insert(MySQLiteHelper.TABLE_CATEGORY, null, values);
+	}
+	
+	/**
+	 * Updates a category in the table.
+	 * 
+	 * @param category
+	 *            the category to be updated
+	 * @return the number of rows updated
+	 */
+	public long update(Category category) {
+		ContentValues values = new ContentValues();
+		values.put(MySQLiteHelper.CATEGORY_NAME, category.getName());
+
+		long result = database.update(MySQLiteHelper.TABLE_CATEGORY, values,
+				WHERE_ID_EQUALS, new String[] { String.valueOf(category.getId()) });
+		Log.d("Update Result:", "=" + result);
+		return result;
+	}
+	
+	/**
+	 * Deletes a category from the table.
+	 * 
+	 * @param category
+	 *            the category to be deleted
+	 * @return the number of rows deleted
+	 */
+	public int deleteCategory(Category category) {
+
+		// NYI DELETE MAPPINGS TO BOOKS FOR THIS CATEGORY
+
+		return database.delete(MySQLiteHelper.TABLE_CATEGORY, WHERE_ID_EQUALS,
+				new String[] { category.getId() + "" });
+	}
+	
+	/**
+	 * Fetches all Categories in the database into a list
+	 * 
+	 * @return a list of all categories in the database
+	 */
+	public List<Category> getAllCategories() {
+		List<Category> categories = new ArrayList<Category>();
+
+		Cursor cursor = database.query(MySQLiteHelper.TABLE_CATEGORY, allColumns,
+				null, null, null, null, null);
+
+		// fill the list with Categories created from rows in the query
+		while (cursor.moveToNext()) {
+			Category category = cursorToCategory(cursor);
+			categories.add(category);
+		}
+		cursor.close();
+
+		return categories;
+	}
+
+	/**
+	 * Creates a Category object from the current cursor location
+	 * 
+	 * @param cursor
+	 *            a cursor pointing at a row in the Category table
+	 * @return a Category object filled in with the information from the table row
+	 */
+	private Category cursorToCategory(Cursor cursor) {
+		Category category = new Category();
+		category.setId(cursor.getLong(MySQLiteHelper.COLUMN_ID_INDEX));
+		category.setName(cursor.getString(MySQLiteHelper.COLUMN_NAME_INDEX));
+		return category;
+	}
+
+}
