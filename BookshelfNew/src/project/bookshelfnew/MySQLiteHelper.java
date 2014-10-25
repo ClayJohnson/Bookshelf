c1404da85e58326cf936f9595504079c40238732
ClayJohnson <imm589@my.utsa.edu>
2014-10-24 19:43:24 -0500

    Removed dead imports from MySQLiteHelper.

--- BookshelfNew/src/project/bookshelfnew/MySQLiteHelper.java
+++ BookshelfNew/src/project/bookshelfnew/MySQLiteHelper.java
@@ -4,9 +4,7 @@
 package project.bookshelfnew;
 
 import android.content.Context;
-import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
