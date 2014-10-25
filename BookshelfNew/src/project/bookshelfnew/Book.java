e1f00c9c0a77390f741dbd6ac18395d161f6bcf1
ClayJohnson <imm589@my.utsa.edu>
2014-10-24 19:20:16 -0500

    Added Parcelable support to Category + hashCode() and equals() methods to Book and Category

--- BookshelfNew/src/project/bookshelfnew/Book.java
+++ BookshelfNew/src/project/bookshelfnew/Book.java
@@ -107,20 +107,39 @@ public class Book implements Parcelable {
 	};
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + (int) (bookmark ^ (bookmark >>> 32));
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
 	public boolean equals(Object obj) {
-		if (this == obj) {
+		if (this == obj)
 			return true;
-		}
-		if (obj == null) {
+		if (obj == null)
 			return false;
-		}
-		if (getClass() != obj.getClass()) {
+		if (getClass() != obj.getClass())
 			return false;
-		}
 		Book other = (Book) obj;
-		if (id != other.id) {
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
 			return false;
-		}
+		if (bookmark != other.bookmark)
+			return false;
+		if (id != other.id)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
 		return true;
 	}
 
