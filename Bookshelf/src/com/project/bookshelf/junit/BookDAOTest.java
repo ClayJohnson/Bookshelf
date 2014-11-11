908e33429bd6e504f9e576b6e072684ec778855d
ClayJohnson <imm589@my.utsa.edu>
2014-11-11 16:22:27 -0600

    generated comments

--- Bookshelf/src/com/project/bookshelf/junit/BookDAOTest.java
+++ Bookshelf/src/com/project/bookshelf/junit/BookDAOTest.java
@@ -1,3 +1,6 @@
+/**
+ * 
+ */
 package com.project.bookshelf.junit;
 
 import static org.junit.Assert.*;
@@ -6,59 +9,96 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * @author Clay
+ *
+ */
 public class BookDAOTest {
 
 	public BookDAOTest() {
 	}
 
+	/**
+	 * @throws java.lang.Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
 	}
 
+	/**
+	 * @throws java.lang.Exception
+	 */
 	@After
 	public void tearDown() throws Exception {
 	}
 
+	/**
+	 * Test method for {@link com.project.bookshelf.database.BookDAO#BookDAO(android.content.Context)}.
+	 */
 	@Test
 	public void testBookDAO() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * Test method for {@link com.project.bookshelf.database.BookDAO#save(com.project.bookshelf.model.Book)}.
+	 */
 	@Test
 	public void testSave() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * Test method for {@link com.project.bookshelf.database.BookDAO#update(com.project.bookshelf.model.Book)}.
+	 */
 	@Test
 	public void testUpdate() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * Test method for {@link com.project.bookshelf.database.BookDAO#deleteBook(com.project.bookshelf.model.Book)}.
+	 */
 	@Test
 	public void testDeleteBook() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * Test method for {@link com.project.bookshelf.database.BookDAO#addBookToCategory(com.project.bookshelf.model.Book, com.project.bookshelf.model.Category)}.
+	 */
 	@Test
 	public void testAddBookToCategory() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * Test method for {@link com.project.bookshelf.database.BookDAO#getBooksByAuthor(java.lang.String)}.
+	 */
 	@Test
 	public void testGetBooksByAuthor() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * Test method for {@link com.project.bookshelf.database.BookDAO#getBooksByTitle(java.lang.String)}.
+	 */
 	@Test
 	public void testGetBooksByTitle() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * Test method for {@link com.project.bookshelf.database.BookDAO#getBooksByCategory(com.project.bookshelf.model.Category)}.
+	 */
 	@Test
 	public void testGetBooksByCategory() {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * Test method for {@link com.project.bookshelf.database.BookDAO#getAllBooks()}.
+	 */
 	@Test
 	public void testGetAllBooks() {
 		fail("Not yet implemented");
