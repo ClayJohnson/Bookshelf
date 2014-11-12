a47b4e707d4a7eac5f07d977e35f212068a5692a
ClayJohnson <imm589@my.utsa.edu>
2014-11-12 14:57:08 -0600

    early draft of BookDAO unit tests.  Still need to mock supporting database entries

--- BookshelfTest/src/com/project/bookshelf/test/BookDAOTest.java
+++ BookshelfTest/src/com/project/bookshelf/test/BookDAOTest.java
@@ -3,15 +3,26 @@
  */
 package com.project.bookshelf.test;
 
-import com.project.bookshelf.database.BookDAO;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.project.bookshelf.database.BookDAO;
+import com.project.bookshelf.database.MySQLiteHelper;
+import com.project.bookshelf.model.Book;
+import com.project.bookshelf.model.Category;
+
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.test.AndroidTestCase;
+import android.test.RenamingDelegatingContext;
 
 /**
  * @author Clay
- *
+ * 
  */
 public class BookDAOTest extends AndroidTestCase {
+
+	private BookDAO bookDAO;
 
 	/**
 	 * @param name
@@ -20,81 +31,208 @@ public class BookDAOTest extends AndroidTestCase {
 		super();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see android.test.AndroidTestCase#setUp()
 	 */
 	protected void setUp() throws Exception {
 		super.setUp();
+		RenamingDelegatingContext context = new RenamingDelegatingContext(
+				getContext(), "test_");
+		bookDAO = new BookDAO(context);
+		bookDAO.open();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see android.test.AndroidTestCase#tearDown()
 	 */
 	protected void tearDown() throws Exception {
+		bookDAO.close();
 		super.tearDown();
 	}
 
+	public void testPreConditions() {
+		assertNotNull(bookDAO);
+	}
+
 	/**
-	 * Test method for {@link com.project.bookshelf.database.BookDAO#BookDAO(android.content.Context)}.
+	 * Test method for
+	 * {@link com.project.bookshelf.database.BookDAO#BookDAO(android.content.Context)}
+	 * .
 	 */
 	public void testBookDAO() {
 		fail("Not yet implemented");
 	}
 
 	/**
-	 * Test method for {@link com.project.bookshelf.database.BookDAO#insertBook(com.project.bookshelf.model.Book)}.
+	 * Test method for
+	 * {@link com.project.bookshelf.database.BookDAO#insertBook(com.project.bookshelf.model.Book)}
+	 * .
 	 */
-	public void testSave() {
-		fail("Not yet implemented");
+	public void testInsertBook() {
+		Book goodBook = new Book();
+		goodBook.setAuthor("author");
+		goodBook.setFileName("fileName");
+		goodBook.setTitle("title");
+		goodBook.setBookmark(-1);
+
+		// try insert good book
+		long insertedRows = 0;
+		try {
+			insertedRows = bookDAO.insertBook(goodBook);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+		assertFalse("insertBook should return a positive number of inserted rows when successful", 
+				insertedRows == 0);
 	}
 
 	/**
-	 * Test method for {@link com.project.bookshelf.database.BookDAO#updateBook(com.project.bookshelf.model.Book)}.
+	 * Test method for
+	 * {@link com.project.bookshelf.database.BookDAO#updateBook(com.project.bookshelf.model.Book)}
+	 * .
 	 */
-	public void testUpdate() {
-		fail("Not yet implemented");
+	public void testUpdateBook() {
+		Book goodBook = new Book();
+		goodBook.setAuthor("author");
+		goodBook.setFileName("fileName");
+		goodBook.setTitle("title");
+		goodBook.setBookmark(-1);
+
+		// try insert good book
+		long updatedRows = 0;
+		try {
+			updatedRows = bookDAO.updateBook(goodBook);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+		assertFalse("updateBook should return a positive number of updated rows when successful", 
+				updatedRows == 0);
 	}
 
 	/**
-	 * Test method for {@link com.project.bookshelf.database.BookDAO#deleteBook(com.project.bookshelf.model.Book)}.
+	 * Test method for
+	 * {@link com.project.bookshelf.database.BookDAO#deleteBook(com.project.bookshelf.model.Book)}
+	 * .
 	 */
 	public void testDeleteBook() {
-		fail("Not yet implemented");
+		Book goodBook = new Book();
+		goodBook.setAuthor("author");
+		goodBook.setFileName("fileName");
+		goodBook.setTitle("title");
+		goodBook.setBookmark(-1);
+
+		// try delete good book
+		long deletedRows = 0;
+		try {
+			deletedRows = bookDAO.deleteBook(goodBook);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+		assertFalse("deleteBook should return a positve number of deleted rows when successul", 
+				deletedRows == 0);
 	}
 
 	/**
-	 * Test method for {@link com.project.bookshelf.database.BookDAO#addBookToCategory(com.project.bookshelf.model.Book, com.project.bookshelf.model.Category)}.
+	 * Test method for
+	 * {@link com.project.bookshelf.database.BookDAO#addBookToCategory(com.project.bookshelf.model.Book, com.project.bookshelf.model.Category)}
+	 * .
 	 */
 	public void testAddBookToCategory() {
-		fail("Not yet implemented");
+		Book goodBook = new Book();
+		goodBook.setAuthor("author");
+		goodBook.setFileName("fileName");
+		goodBook.setTitle("title");
+		goodBook.setBookmark(-1);
+
+		Category goodCategory = new Category();
+		goodCategory.setName("name");
+
+		long id = -1;
+		try {
+			id = bookDAO.addBookToCategory(goodBook, goodCategory);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+		assertFalse("addBookToCategory should return the id of the mapping if successful", 
+				id == -1);
 	}
 
 	/**
-	 * Test method for {@link com.project.bookshelf.database.BookDAO#getBooksByAuthor(java.lang.String)}.
+	 * Test method for
+	 * {@link com.project.bookshelf.database.BookDAO#getBooksByAuthor(java.lang.String)}
+	 * .
 	 */
 	public void testGetBooksByAuthor() {
-		fail("Not yet implemented");
+		String author = "author";
+
+		List<Book> books = null;
+		try {
+			books = bookDAO.getBooksByAuthor(author);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+		assertNotNull("getBooksByAuthor should return a list of Books when successful", books);
 	}
 
 	/**
-	 * Test method for {@link com.project.bookshelf.database.BookDAO#getBooksByTitle(java.lang.String)}.
+	 * Test method for
+	 * {@link com.project.bookshelf.database.BookDAO#getBooksByTitle(java.lang.String)}
+	 * .
 	 */
 	public void testGetBooksByTitle() {
-		fail("Not yet implemented");
+		String title = "title";
+
+		List<Book> books = null;
+		try {
+			books = bookDAO.getBooksByTitle(title);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+		assertNotNull("getBooksByTitle should return a list of Books when successful", books);
 	}
 
 	/**
-	 * Test method for {@link com.project.bookshelf.database.BookDAO#getBooksByCategory(com.project.bookshelf.model.Category)}.
+	 * Test method for
+	 * {@link com.project.bookshelf.database.BookDAO#getBooksByCategory(com.project.bookshelf.model.Category)}
+	 * .
 	 */
 	public void testGetBooksByCategory() {
-		fail("Not yet implemented");
+		Category goodCategory = new Category();
+		goodCategory.setName("name");
+
+		List<Book> books = null;
+		try {
+			books = bookDAO.getBooksByCategory(goodCategory);
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+		assertNotNull("getBooksByCategory should return a list of Books when successful", books);
 	}
 
 	/**
-	 * Test method for {@link com.project.bookshelf.database.BookDAO#getAllBooks()}.
+	 * Test method for
+	 * {@link com.project.bookshelf.database.BookDAO#getAllBooks()}.
 	 */
 	public void testGetAllBooks() {
-		fail("Not yet implemented");
+		List<Book> books = null;
+		try {
+			books = bookDAO.getAllBooks();
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+		assertNotNull("getAllBooks should return a list of Books when successful", books);
 	}
 
 }
