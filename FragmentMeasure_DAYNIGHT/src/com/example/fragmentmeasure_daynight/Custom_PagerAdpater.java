ff89f42968d48943a731e452595b9d11167c53e0
anjuchatta10 <anju.chatta@gmail.com>
2014-11-13 10:06:46 -0600

    FragmentMeasure_DAYNIGHT
    New project added with all modifciations done. The porject opens .txt
    file and splits, displays file contents across all pages and the DAY
    NIGHT mode buttons change the page color font.

---
+++ FragmentMeasure_DAYNIGHT/src/com/example/fragmentmeasure_daynight/Custom_PagerAdpater.java
@@ -0,0 +1,47 @@
+package com.example.fragmentmeasure_daynight;
+
+/*Author Anjana Chatta*/
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.Hashtable;
+import java.util.List;
+
+public class Custom_PagerAdpater extends FragmentPagerAdapter {
+
+	private final List<CharSequence> pageTexts;
+
+	Hashtable<Integer, Fragment> mPageReferenceMap = new Hashtable<Integer, Fragment>();
+
+	public Custom_PagerAdpater(FragmentManager fm, List<CharSequence> pageTexts) {
+		super(fm);
+		this.pageTexts = pageTexts;
+
+	}
+
+	@Override
+	public Fragment getItem(int i) {
+		// return Fragments.newInstance(pageTexts.get(i));
+
+		Fragment frag = Fragments.newInstance(pageTexts.get(i));
+		mPageReferenceMap.put(i, frag);
+		return frag;
+	}
+
+	// new
+	public Fragment getFragment(int key) {
+
+		return mPageReferenceMap.get(key);
+
+	}
+
+	//
+
+	@Override
+	public int getCount() {
+		return pageTexts.size();
+	}
+
+}
---
+++ FragmentMeasure_DAYNIGHT/src/com/example/fragmentmeasure_daynight/Fragments.java
@@ -0,0 +1,56 @@
+package com.example.fragmentmeasure_daynight;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Fragments extends Fragment implements View.OnClickListener {
+	private final static String strText = "TEXT";
+	TextView textView;
+	public Button btnDay, btnNight;
+
+	public static Fragments newInstance(CharSequence pageText) {
+		Fragments fragments = new Fragments();
+		Bundle bundle = new Bundle();
+		bundle.putCharSequence(strText, pageText);
+		fragments.setArguments(bundle);
+		return fragments;
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		CharSequence pagetext = getArguments().getCharSequence(strText);
+		View view = inflater.inflate(R.layout.page, container, false);
+		textView = (TextView) inflater.inflate(R.layout.page, container, false);
+		textView.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources()
+				.getDimension(R.dimen.text_size));
+		textView.setText(pagetext);
+		return textView;
+	}
+
+	public void changeDAYColor() {
+		textView.setTextColor(Color.BLACK);
+		textView.setBackgroundColor(Color.WHITE);
+	}
+
+	public void changeNIGHTColor() {
+
+		textView.setTextColor(Color.WHITE);
+		textView.setBackgroundColor(Color.BLACK);
+
+	}
+
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+	}
+
+}
